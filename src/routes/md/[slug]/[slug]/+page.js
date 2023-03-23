82adfdb5f283ef20b26de4caedb73dd98e4895c7
RomanistHere <RomanistHere@pm.me>
2023-03-23 15:32:06 +0400

    fix: add descriptions seo

--- src/routes/md/[slug]/[slug]/+page.js
+++ src/routes/md/[slug]/[slug]/+page.js
@@ -1,10 +1,11 @@
 export async function load({ params }) {
 	const post = await import(`../../second-layer/${params.slug}.md`);
-	const { title } = post.metadata;
+	const { title, description } = post.metadata;
 	const content = post.default;
 
 	return {
 		content,
 		title,
+		description,
 	};
 }
